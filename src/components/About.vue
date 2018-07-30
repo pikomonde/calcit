ddf0630c888e7266d69414887b166058183d922a
Piko Monde <pikomonde@gmail.com>
2018-07-31 03:21:21 +0700

    add pages on routing

---
+++ src/components/About.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>This is a about page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'About',
+  data () {
+    return {
+      // msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
